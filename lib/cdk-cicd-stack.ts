16a74bffe5a613f7ddd3f014d907df5fd1352382
github-action-learning <roysaurav1987@gmail.com>
2025-12-05 09:30:35 +0100

    commit to git

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,16 +1,29 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib/core";
+import { Construct } from "constructs";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkCicdQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pipeline = new CodePipeline(this, "PracticePipeline", {
+      pipelineName: "PracticePipeline",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub(
+          "Sauravroy1987/CDK-course-resources", // Github Repository
+          "cicd-practice" // Branch Name inside repository. Full root directory 'AWS_CDK_PROJECTS'
+        ),
+        commands: [
+          "cd cdk-cicd", // Just build and deploy the code inside 'cdk-cicd'
+          "npm ci",
+          "npx cdk synth",
+        ], // Commands to execute on code
+        primaryOutputDirectory: "cdk-cicd/cdk.out",
+      }),
+    });
   }
 }
