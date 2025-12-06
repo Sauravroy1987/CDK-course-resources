1f558252ac3dc1f2e03f563f571fcaba6d8c9cd5
github-action-learning <roysaurav1987@gmail.com>
2025-12-06 07:51:07 +0100

    Deliver Lambda Stack

--- lib/LambdaStack.ts
+++ lib/LambdaStack.ts
@@ -1,5 +1,12 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { Duration, Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
+import {
+  Code,
+  Function as LambdaFunction,
+  Runtime,
+  Tracing,
+} from "aws-cdk-lib/aws-lambda";
+import { join } from "path";
 
 // Properties for Stage Name
 interface LambdaStackProps extends StackProps {
@@ -9,5 +16,19 @@ interface LambdaStackProps extends StackProps {
 export class LambdaStack extends Stack {
   constructor(scope: Construct, id: string, props: LambdaStackProps) {
     super(scope, id, props);
+
+    const helloLambda = new LambdaFunction(this, "HelloLambda", {
+      runtime: Runtime.NODEJS_20_X,
+      handler: "hello.handler", // handler function name
+      code: Code.fromAsset(join(__dirname, "..", "services")), // Find /services/hello.js file
+      // Set the table name as environment variable
+      environment: {
+        STAGE: props.stageName,
+      },
+      // Enable X-Ray for tracing lambda execution
+      tracing: Tracing.ACTIVE,
+      // Set lambda excution timeout duration to 1min. Default 3secs.
+      timeout: Duration.minutes(1),
+    });
   }
 }
