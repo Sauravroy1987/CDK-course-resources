578d7fffb86ae210d0555ebb463740ae022bf54f
github-action-learning <roysaurav1987@gmail.com>
2025-12-06 09:09:30 +0100

    Added Unit test-3

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -46,7 +46,7 @@ export class CdkCicdStack extends cdk.Stack {
     // Execute test before the Stage(testStage) start
     testStage.addPre(
       new CodeBuildStep("Unit test", {
-        commands: ["cd cdk-cicd", "npm ci", "npm test"],
+        commands: ["npm ci", "npm test"],
       })
     );
   }
