6a42cc3749957189d9008a5ae0642bc496f87e89
github-action-learning <roysaurav1987@gmail.com>
2025-12-06 08:52:44 +0100

    Added Unit test

--- test/cdk-cicd.test.ts
+++ test/cdk-cicd.test.ts
@@ -6,6 +6,6 @@ describe("Hello describe test suite", () => {
     const result = await handler({} as any, {} as any);
 
     // Assertion, the fetch method will call once
-    expect(result.statusCode).toBe(200);
+    expect(result.statusCode).toBe(202);
   });
 });
